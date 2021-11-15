ae5cfef28e7fdd9aab71c5a7c0f314d363e2ab47
Peter Chen <peter_chen@trimble.com>
2021-11-14 21:37:49 -0700

    Add player camera

--- Client/Game/Player.cs
+++ Client/Game/Player.cs
@@ -8,8 +8,13 @@ using EventHorizon.Blazor.Interop;
 [JsonConverter(typeof(CachedEntityConverter<Player>))]
 public class Player : TransformNode
 {
+    private static readonly Vector3 ORIGINAL_TILT = new(0.5934119456780721m, 0, 0);
+
     private readonly Scene _scene;
     private readonly Mesh _mesh;
+    private TransformNode _cameraRoot;
+    private TransformNode _yTilt;
+    private UniversalCamera _camera;
 
     public Player(
         IDictionary<string, Mesh> assets,
@@ -30,15 +35,26 @@ public class Player : TransformNode
 
     private void SetupPlayerCamera()
     {
-        var camera4 = new ArcRotateCamera(
-            "arc",
-            -(decimal)System.Math.PI / 2,
-            (decimal)System.Math.PI / 2,
-            radius: 40,
-            new Vector3(0, 3, 0),
-            _scene
-        );
-        _scene.activeCamera = camera4;
-        camera4.attachControl(true);
+        _cameraRoot = new TransformNode("root")
+        {
+            position = Vector3.Zero(),
+            rotation = new Vector3(0, (decimal)System.Math.PI, 0)
+        };
+
+        _yTilt = new TransformNode("y-tilt")
+        {
+            rotation = ORIGINAL_TILT,
+            parent = _cameraRoot
+        };
+
+        _camera = new UniversalCamera("cam", new Vector3(0, 0, -30), _scene)
+        {
+            lockedTarget = _cameraRoot.position,
+            fov = 0.47m,
+            parent = _yTilt
+        };
+
+        _scene.activeCamera = _camera;
+        _camera.attachControl(true);
     }
 }
